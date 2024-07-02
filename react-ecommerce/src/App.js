62c8d80f35ff718c20327467004eb765ba2e4622
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-07-02 15:40:38 +0530

    add react-route

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Home from './pages/Home';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import { Login } from "./features/auth/components/Login";
+import { Signup } from "./features/auth/components/Signup";
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home></Home>
+  },
+  {
+    path: "/login",
+    element: <Login></Login>
+  },
+  {
+    path: "/signup",
+    element: <Signup></Signup>
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+      <RouterProvider router={router} />
     </div>
   );
 }
